a00e98ed8a297e8a7cd70aababad2f7a5e7cfcd0
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2021-09-21 11:26:15 +0200

    Database Initial Commit

---
+++ DB/DB.php
@@ -0,0 +1,29 @@
+<?
+
+namespace DB;
+
+use PDO;
+
+$host="localhost";
+    $db_name="";
+    $usr="root";
+    $pass="";
+    class DB extends PDO
+    {
+        private $host;
+        private $db_name;
+        private $user;
+        private $pass;
+        private $conn_str;
+
+        public function __construct($host="localhost",$db_name="project",$user="root",$password="")
+        {
+            $this->host=$host;
+            $this->db_name=$db_name;
+            $this->user=$user;
+            $this->pass=$password;
+            $this->conn_str="mysql:host=".$this->host.";dbname=".$this->db_name;
+            parent::__construct($this->conn_str,$this->user,$this->pass);
+        }
+    }
+?>
