d3658035724e8d3b26471736b8a1e66d20cca369
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2021-09-21 14:51:36 +0200

    API Not finished headers, et cetera.

---
+++ API/Index.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace API;
+
+//
+
+//headers
+
+$usr=new User();
+
+switch($_SERVER['REQUEST_METHOD']){
+    case "GET":
+        $data=$usr->read($_GET['id']);
+        $data=array(
+            'name' => $name,
+            'lastname' =>
+            'role' =>
+            'group' =>
+        );
+        $response=json_encode($data);
+        break;
+    case "DELETE":
+        if(isset($_GET['id']))
+        {
+            if($usr->read($_GET['id']))
+            {
+                $usr->delete($_GET['id']);
+            }
+            else break;//response
+        }
+        else break;//response
+        break;
+    case "PUT":
+        if(isset($_GET['id'])) $id=$_GET['id'];
+        else break;//response
+        if(!$usr->read($id)) break;//response
+        $json=file_get_contents("php://input");
+        $data=json_decode($json);
+        if( isset($data['name'])||
+            isset($data['lastname'])||
+            isset($data['role_id'])||
+            isset($data['group_id']))
+            $usr->update($data['name'],$data['lastname'],$data['role_id'],$data['group_id']);
+        else break;
+        //response
+        break;
+    case "POST":
+        $json=file_get_contents("php://input");
+        $data=json_decode($json);
+        if( isset($data['name'])||
+            isset($data['lastname'])||
+            isset($data['role_id'])||
+            isset($data['group_id']))
+            {
+                $usr->create($data['name'],$data['lastname'],$data['role_id'],$data['group_id']);
+                //response 200
+            }
+        break;
+    }
+
+?>
