cb83d5636de6b8d3fb824efb43357446212cef5a
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2021-09-21 11:31:10 +0200

    initial core

---
+++ core/Mentor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace core;
+
+class Mentor
+{
+    public function __construct();
+}
---
+++ core/User.php
@@ -0,0 +1,13 @@
+<?
+
+namespace core;
+
+interface User
+{
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
+}
+
+?>
